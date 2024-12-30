79bbb1e708aafcc80eadc7860594dd6bf293057f
xmhu2001 <2268104085@qq.com>
2024-12-30 22:42:55 +0800

    finish lesson 19

--- exercises/16_class_move/main.cpp
+++ exercises/16_class_move/main.cpp
@@ -1,5 +1,5 @@
 #include "../exercise.h"
-
+#include <utility>
 // READ: 左值右值（概念）<https://learn.microsoft.com/zh-cn/cpp/c-language/l-value-and-r-value-expressions?view=msvc-170>
 // READ: 左值右值（细节）<https://zh.cppreference.com/w/cpp/language/value_category>
 // READ: 关于移动语义 <https://learn.microsoft.com/zh-cn/cpp/cpp/rvalue-reference-declarator-amp-amp?view=msvc-170#move-semantics>
@@ -22,8 +22,10 @@ public:
     }
 
     // TODO: 实现移动构造器
-    DynFibonacci(DynFibonacci &&other) noexcept: cache(std::exchange(other.cache, nullptr)), cached(std::exchange(other.cached, 0)) {
+    DynFibonacci(DynFibonacci &&other) noexcept: cache(other.cache), cached(other.cached) {
         std::cout << "move constructor called" << std::endl;
+        other.cache = nullptr;
+        other.cached = 0;
     }
 
     // TODO: 实现移动赋值
