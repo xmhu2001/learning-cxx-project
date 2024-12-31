d57994a0bde2ee2ef5d5cc57cd8c006abe58a12b
xmhu2001 <2268104085@qq.com>
2024-12-31 21:38:57 +0800

    finish lesson 30-31

--- exercises/30_std_unique_ptr/main.cpp
+++ exercises/30_std_unique_ptr/main.cpp
@@ -53,16 +53,18 @@ int main(int argc, char **argv) {
         {"fd"},
         // TODO: 分析 problems[1] 中资源的生命周期，将记录填入 `std::vector`
         // NOTICE: 此题结果依赖对象析构逻辑，平台相关，提交时以 CI 实际运行平台为准
-        {"", "", "", "", "", "", "", ""},
-        {"", "", "", "", "", "", "", ""},
+        {"ffr", "d"},
+        {"r", "d", "d"},
     };
 
     // ---- 不要修改以下代码 ----
 
     for (auto i = 0; i < 3; ++i) {
         ASSERT(problems[i].size() == answers[i].size(), "wrong size");
+        std::cout << "---------" << std::endl;
         for (auto j = 0; j < problems[i].size(); ++j) {
             ASSERT(std::strcmp(problems[i][j].c_str(), answers[i][j]) == 0, "wrong location");
+            std::cout << problems[i][j].c_str() << std::endl;
         }
     }
 
